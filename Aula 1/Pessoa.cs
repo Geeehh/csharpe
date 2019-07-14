89b7988caa1efda0bdbe32b58d854f6c3554596b
Géssica Da Silva Siqueira <66109@PROWAY.TREINA>
2019-07-14 14:59:58 -0300

    Projeto aula 1

---
+++ Aula 1/Pessoa.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula_1
+{
+    class Pessoa
+    {
+        public string Nome { get; set; }
+        public string NumeroTelefone { get; set; }
+        public string Arquivo { get; set; }
+
+        public Pessoa()
+        {
+            Arquivo = @"\\svprwy001\Alunos\AlunosMelhorLinguagem\ListaDeContatos.txt";
+        }
+
+        public string DadosPessoa()
+        {
+            string pessoaDados = Nome + "-"  + NumeroTelefone;
+
+            return pessoaDados;
+        }  
+
+        public void Gravar(Pessoa p)
+        {
+            StreamWriter stream;
+            stream = File.AppendText(Arquivo);
+            stream.WriteLine(p.DadosPessoa() );
+            stream.Close();
+
+
+        }
+
+        public List<Pessoa> Ler()
+        {
+            List<Pessoa> pessoas = new List<Pessoa>();
+
+            StreamReader stream;
+            stream = File.OpenText(Arquivo);
+            while (stream.EndOfStream == false)
+            {
+                string linha = stream.ReadLine();
+                string[] pessoaLinha = linha.Split('-');
+                Pessoa pessoa = new Pessoa();
+                pessoa.Nome = pessoaLinha[0];
+                pessoa.NumeroTelefone = pessoaLinha[1];
+                pessoas.Add(pessoa);
+
+
+            }
+            stream.Close();
+            return pessoas;
+
+            //pessoa1.Nome = "Joao";
+            //pessoa1.NumeroTelefone = "9999-9999";
+
+            //Pessoa pessoa2 = new Pessoa();
+            //pessoa2.Nome = "Maria";
+            //pessoa2.NumeroTelefone = "5555-5555";
+
+            //pessoas.Add(pessoa1);
+            //pessoas.Add(pessoa2);
+
+                
+        }
+
+
+
+    }
+}
---
+++ Aula 1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lista de Alunos");
+
+            Pessoa pessoa = new Pessoa();
+
+
+            Console.Write("Digite o nome: ");
+            pessoa.Nome = Console.ReadLine();
+
+            Console.Write("Digite o numero do  telefone: ");
+            pessoa.NumeroTelefone = Console.ReadLine();
+
+            string dados = pessoa.DadosPessoa();
+            Console.WriteLine(dados);
+
+            pessoa.Gravar(pessoa);
+
+            //List<Pessoa> pessoas = pessoa.Ler();
+
+            //foreach (Pessoa p in pessoas)
+            //{
+            //    Console.WriteLine(p.DadosPessoa());
+
+            //}
+
+
+            Console.ReadLine();
+
+            
+
+
+
+
+        }
+    
+    }
+}
